d9b61b749e08fa171c01524fccee286c8fdddd44
Andi <familie.mayer@aon.at>
2020-01-28 20:59:33 +0100

    removed api key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: 'AIzaSyBtoMJySy34PJR8B_Mrs8OZJR5z8mNsKsc',
     authDomain: 'showc-39179.firebaseapp.com',
     databaseURL: 'https://showc-39179.firebaseio.com',
     projectId: 'showc-39179',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,6 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyBtoMJySy34PJR8B_Mrs8OZJR5z8mNsKsc',
     authDomain: 'showc-39179.firebaseapp.com',
     databaseURL: 'https://showc-39179.firebaseio.com',
     projectId: 'showc-39179',
